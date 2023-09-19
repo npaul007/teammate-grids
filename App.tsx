3c86eec980fbd99c93accceca50f3f8653c4183c
npaul008 <nathanael.paulemon@geniussports.com>
2023-09-19 10:04:37 -0400

    add auth route handling

--- App.tsx
+++ App.tsx
@@ -1,17 +1,41 @@
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
+import React, { useEffect, useState } from "react";
+import { Exist } from "./components";
+import { getAuthCookie } from "./modules/utils";
 import { Game, Login, Register } from "./screens";
 
 const Stack = createStackNavigator();
 
 export default function App() {
+  const [isSignedIn, setIsSignedIn] = useState(false);
+
+  useEffect(() => {
+    (async () => {
+      const token = await getAuthCookie();
+      if (token) {
+        setIsSignedIn(true);
+      } else {
+        setIsSignedIn(false);
+      }
+    })();
+
+    return () => {};
+  }, [isSignedIn, setIsSignedIn]);
+
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Login">
-        <Stack.Screen name="Login" component={Login} />
-        <Stack.Screen name="Register" component={Register} />
-        <Stack.Screen name="Game" component={Game} />
-      </Stack.Navigator>
+      <Exist when={isSignedIn}>
+        <Stack.Navigator initialRouteName="Game">
+          <Stack.Screen name="Game" component={Game} />
+        </Stack.Navigator>
+      </Exist>
+      <Exist when={!isSignedIn}>
+        <Stack.Navigator initialRouteName="Login">
+          <Stack.Screen name="Login" component={Login} />
+          <Stack.Screen name="Register" component={Register} />
+        </Stack.Navigator>
+      </Exist>
     </NavigationContainer>
   );
 }
